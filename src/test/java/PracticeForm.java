c4e2fd77a5f3ca2358a8e81bff54c19f27e471ec
kolomiets.aa <kolomiets.aan@gazprom-neft.ru>
2025-06-18 13:14:42 +0300

    промежуточный коммит

--- src/test/java/PracticeForm.java
+++ src/test/java/PracticeForm.java
@@ -1,2 +1,71 @@
-package PACKAGE_NAME;public class PracticeForm {
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class PracticeForm {
+
+    @BeforeAll
+    static void basicBrowserSettings() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void fullFormTest() {
+        open("/automation-practice-form");
+
+        $("#firstName").setValue("Artem");
+        $("#lastName").setValue("Pupkin");
+
+        $("#userEmail").setValue("l25dscx@mail.ru");
+
+        $(byText("Female")).click();
+
+        $("#userNumber").setValue("9333494066");
+
+        $("#dateOfBirthInput").clear();
+        $(".react-datepicker__month-select").selectOption("July");
+        $(".react-datepicker__year-select").selectOption("2025");
+        $$(".react-datepicker__day").findBy(text("18")).click();
+
+        $("#subjectsInput").setValue("Math");
+        $$(".subjects-auto-complete__option").findBy(text("Maths")).click();
+        $("#subjectsInput").setValue("Phy");
+        $$(".subjects-auto-complete__option").findBy(text("Physics")).click();
+        $("#subjectsInput").setValue("Eng");
+        $$(".subjects-auto-complete__option").findBy(text("English")).click();
+
+        $("label[for='hobbies-checkbox-1']").click();
+        $("label[for='hobbies-checkbox-3']").click();
+
+        $("#uploadPicture").uploadFromClasspath("1.jpg");
+
+        $("#currentAddress")
+                .scrollTo()
+                .shouldBe(visible, enabled)
+                .setValue("Russia, SPB");
+
+        // Блок State and City
+        $("#react-select-3-input").setValue("NCR").pressEnter();
+        $("#react-select-4-input").setValue("Delhi").pressEnter();
+        $("#submit").pressEnter();
+
+
+        // Проверка текста в форме после заполнения
+        $(".table-responsive").shouldHave(text("Artem Pupkin"));
+        $(".table-responsive").shouldHave(text("l25dscx@mail.ru"));
+        $(".table-responsive").shouldHave(text("Female"));
+        $(".table-responsive").shouldHave(text("9333494066"));
+        $(".table-responsive").shouldHave(text("18 July,2025"));
+        $(".table-responsive").shouldHave(text("Maths, Physics, English"));
+        $(".table-responsive").shouldHave(text("Sports, Music"));
+        $(".table-responsive").shouldHave(text("1.jpg"));
+        $(".table-responsive").shouldHave(text("Russia, SPB"));
+        $(".table-responsive").shouldHave(text("NCR Delhi"));
+    }
 }
