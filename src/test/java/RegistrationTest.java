790858bc4cf9fbf508a72f98922851e2af2da7df
kolomiets.aa <kolomiets.aan@gazprom-neft.ru>
2025-07-24 13:05:50 +0300

    оптимизировал тесты

--- src/test/java/RegistrationTest.java
+++ src/test/java/RegistrationTest.java
@@ -1,63 +1,79 @@
+import data.CalendarDate;
+import data.DataGeneration;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import pages.RegistrationPages;
 
 public class RegistrationTest extends TestBase {
+
+    private DataGeneration data;
+    private final RegistrationPages.ResultModal resultModal = new RegistrationPages.ResultModal();
+    private final RegistrationPages registrationPages = new RegistrationPages();
+
+    @BeforeEach
+    public void initTestData() {
+        data = new DataGeneration();
+    }
 
     @Test
     void successfulFillFormTest() {
 
         registrationPages.openPage()
-                .setFirstName("Artem")
-                .setLastName("Pupkin")
-                .setEmail("l25dscx@mail.ru")
-                .setGender("Male")
-                .setNumber("9333494066")
-                .setBirthDate("28", "June", "1992")
-                .setSubjects("Maths")
-                .setHobbies("Sports")
-                .uploadPicture("1.jpg")
-                .setAddress("Russia SPB")
-                .selectStateAndCity("NCR","Delhi" )
+                .setFirstName(data.getFirstName())
+                .setLastName(data.getLastName())
+                .setEmail(data.getEmail())
+                .setGender(data.getGender())
+                .setMobile(data.getMobile())
+                .selectDateOfBirth(data.getDateOfBirth())
+                .selectSubjects(data.getSubjects())
+                .selectHobbies(data.getHobbies())
+                .uploadPicture(data.getFilename())
+                .setAddress(data.getCurrentAddress())
+                .selectState(data.getState())
+                .selectCity(data.getCity())
                 .submitForm();
 
-        registrationPages.verifyResultsModalAppears()
-                .verifyResult("Student Name", "Artem" + " Pupkin")
-                .verifyResult("Student Email", "l25dscx@mail.ru")
-                .verifyResult("Gender", "Male")
-                .verifyResult("Mobile", "9333494066")
-                .verifyResult("Date of Birth", "28 June,1992")
-                .verifyResult("Subjects", "Maths")
-                .verifyResult("Hobbies", "Sports")
-                .verifyResult("Picture", "1.jpg")
-                .verifyResult("Address", "Russia SPB")
-                .verifyResult("State and City", "NCR Delhi");
+        resultModal.shouldHaveStudentName(data.getFirstName(), data.getLastName())
+                .shouldHaveStudentName(data.getFirstName(), data.getLastName())
+                .shouldHaveStudentEmail(data.getEmail())
+                .shouldHaveGender(data.getGender())
+                .shouldHaveMobile(data.getMobile())
+                .shouldHaveDateOfBirth(data.getDateOfBirth())
+                .shouldHaveSubjects(data.getSubjects())
+                .shouldHaveHobbies(data.getHobbies())
+                .shouldHavePicture(data.getFilename())
+                .shouldHaveAddress(data.getCurrentAddress())
+                .shouldHaveStateAndCity(data.getState(), data.getCity());
     }
 
     @Test
     void successfulMinFormTest() {
 
         registrationPages.openPage()
-                .setFirstName("Artem")
-                .setLastName("Pupkin")
-                .setGender("Male")
-                .setNumber("9333494066")
+                .setFirstName(data.getFirstName())
+                .setLastName(data.getLastName())
+                .setGender(data.getGender())
+                .setMobile(data.getMobile())
                 .submitForm();
 
-        registrationPages.verifyResultsModalAppears()
-                .verifyResult("Student Name", "Artem" + " Pupkin")
-                .verifyResult("Gender", "Male")
-                .verifyResult("Mobile", "9333494066");
+        resultModal.shouldHaveStudentName(data.getFirstName(), data.getLastName())
+                .shouldHaveGender(data.getGender())
+                .shouldHaveMobile(data.getMobile())
+                .shouldHaveDateOfBirth(CalendarDate.now())
+                .shouldHaveStudentEmail("")
+                .shouldHaveSubjects("")
+                .shouldHaveHobbies("")
+                .shouldHavePicture("")
+                .shouldHaveAddress("")
+                .shouldHaveStateAndCity("", "");
     }
 
     @Test
     void negativeMinFormTest() {
 
         registrationPages.openPage()
-                .setFirstName("iVAN")
-                .setLastName("Ivanov")
-                .setGender("Male")
-                .setNumber("")
                 .submitForm();
+      resultModal.shouldNotBeVisible();
 
-        registrationPages.verifyResultsModalAppearsNeg();
     }
 }
