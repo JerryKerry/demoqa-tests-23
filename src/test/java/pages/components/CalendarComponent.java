5bc92690ed723ef88bdbf7e8a4db30619c33c5a8
kolomiets.aa <kolomiets.aan@gazprom-neft.ru>
2025-07-24 12:57:19 +0300

    добавлен метод клика на элемент, переделан метод работы с календарём

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -1,11 +1,26 @@
 package pages.components;
 
+import com.codeborne.selenide.SelenideElement;
+import data.CalendarDate;
+
 import static com.codeborne.selenide.Selenide.$;
 
 public class CalendarComponent {
-    public void setDate(String day, String month, String year) {
-        $(".react-datepicker__month-select").selectOption(month);
-        $(".react-datepicker__year-select").selectOption(year);
-        $(".react-datepicker__day--0" + day).click();
+    private final SelenideElement element;
+
+    public CalendarComponent(SelenideElement element) {
+        this.element = element;
+    }
+
+    public void setDate(CalendarDate date) {
+        $(".react-datepicker__year-select").selectOption(date.year());
+        $(".react-datepicker__month-select").selectOption(date.month());
+
+        String formattedDay = String.format("%03d", date.day());
+        $(".react-datepicker__day--" + formattedDay + ":not(.react-datepicker__day--outside-month)").click();
+    }
+
+    public void click() {
+        element.click();
     }
 }
