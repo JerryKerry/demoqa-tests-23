2d1bf0bf8292e5dede56c478a3959717a6621aa0
kolomiets.aa <kolomiets.aan@gazprom-neft.ru>
2025-07-24 13:04:49 +0300

    тут честно подсмотрел реализацию у других однокурсников, остановился на таком варианте

--- src/test/java/pages/RegistrationPages.java
+++ src/test/java/pages/RegistrationPages.java
@@ -1,6 +1,8 @@
 package pages;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import data.CalendarDate;
 import pages.components.CalendarComponent;
 import pages.components.RegistrationResultsModal;
 
@@ -8,8 +10,7 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class RegistrationPages {
-    CalendarComponent calendarComponent = new CalendarComponent();
-    RegistrationResultsModal registrationResultsModal = new RegistrationResultsModal();
+
     private SelenideElement
             firstNameInput = $("#userName-wrapper #firstName"),
             lastNameInput = $("#userName-wrapper #lastName"),
@@ -27,7 +28,6 @@ public class RegistrationPages {
 
     public RegistrationPages openPage() {
         open("/automation-practice-form");
-        //Убираем рекламу
         executeJavaScript("$('footer').remove();");
         executeJavaScript("$('#fixedban').remove();");
 
@@ -64,22 +64,25 @@ public class RegistrationPages {
         return this;
     }
 
-    public RegistrationPages setBirthDate(String day, String month, String year) {
-        dateOfBirthInput.click();
-        calendarComponent.setDate(day, month, year);
+    public RegistrationPages selectDateOfBirth(CalendarDate date) {
+        CalendarComponent calendar = new CalendarComponent(dateOfBirthInput);
+        calendar.click();
+        calendar.setDate(date);
 
         return this;
     }
 
-    public RegistrationPages setSubjects(String value) {
-        subjectsInput.setValue(value).pressEnter();
-
+    public RegistrationPages selectSubjects(String... subjects) {
+        for (String subject : subjects) {
+            subjectsInput.setValue(subject).pressEnter();
+        }
         return this;
     }
 
-    public RegistrationPages setHobbies(String value) {
-        hobbiesInput.$(byText(value)).click();
-
+    public RegistrationPages selectHobbies(String... hobbies) {
+        for (String hobby : hobbies) {
+            hobbiesInput.$(byText(hobby)).click();
+        }
         return this;
     }
 
@@ -95,12 +98,26 @@ public class RegistrationPages {
         return this;
     }
 
-    public RegistrationPages selectStateAndCity(String state, String city) {
-        stateInput.click();
-        $(byText(state)).click();
-        cityInput.click();
-        $(byText(city)).click();
+    public RegistrationPages setMobile(String mobile) {
+        numberInput.setValue(mobile);
+        return this;
+    }
 
+    public RegistrationPages selectState(String state) {
+        stateInput.click();
+        stateInput.$$("div")
+                .filterBy(Condition.exactText(state))
+                .first()
+                .click();
+        return this;
+    }
+
+    public RegistrationPages selectCity(String city) {
+        cityInput.click();
+        cityInput.$$("div")
+                .filterBy(Condition.exactText(city))
+                .first()
+                .click();
         return this;
     }
 
@@ -108,19 +125,72 @@ public class RegistrationPages {
         submitTab.click();
     }
 
-    public RegistrationPages verifyResultsModalAppears() {
-        registrationResultsModal.verifyModalAppears();
+    public static class ResultModal {
+        private static final String STUDENT_NAME_LABEL = "Student Name";
+        private static final String STUDENT_EMAIL_LABEL = "Student Email";
+        private static final String GENDER_LABEL = "Gender";
+        private static final String MOBILE_LABEL = "Mobile";
+        private static final String DATE_OF_BIRTH_LABEL = "Date of Birth";
+        private static final String SUBJECTS_LABEL = "Subjects";
+        private static final String HOBBIES_LABEL = "Hobbies";
+        private static final String PICTURE_LABEL = "Picture";
+        private static final String ADDRESS_LABEL = "Address";
+        private static final String STATE_AND_CITY_LABEL = "State and City";
 
-        return this;
-    }
+        RegistrationResultsModal result = new RegistrationResultsModal();
 
-    public RegistrationPages verifyResult(String key, String value) {
-        registrationResultsModal.verifyResult(key, value);
+        public ResultModal shouldHaveStudentName(String firstNae, String lastNae){
+            result.verifyResult(STUDENT_NAME_LABEL, firstNae + " " + lastNae);
+            return this;
+        };
 
-        return this;
-    }
+        public ResultModal shouldHaveStudentEmail(String email) {
+            result.verifyResult(STUDENT_EMAIL_LABEL, email);
+            return this;
+        }
 
-    public void verifyResultsModalAppearsNeg() {
-        registrationResultsModal.verifyModalAppearsNegativ();
+        public ResultModal shouldHaveGender(String gender) {
+            result.verifyResult(GENDER_LABEL, gender);
+            return this;
+        }
+
+        public ResultModal shouldHaveMobile(String mobile) {
+            result.verifyResult(MOBILE_LABEL, mobile);
+            return this;
+        }
+
+        public ResultModal shouldHaveDateOfBirth(CalendarDate date) {
+            String formattedDate = String.format("%02d %s,%s", date.day(), date.month(), date.year());
+            result.verifyResult(DATE_OF_BIRTH_LABEL, formattedDate);
+            return this;
+        }
+
+        public ResultModal shouldHaveSubjects(String... subjects) {
+            result.verifyResult(SUBJECTS_LABEL, String.join(", ", subjects));
+            return this;
+        }
+
+        public ResultModal shouldHaveHobbies(String... hobbies) {
+            result.verifyResult(HOBBIES_LABEL, String.join(", ", hobbies));
+            return this;
+        }
+
+        public ResultModal shouldHavePicture(String filename) {
+            result.verifyResult(PICTURE_LABEL, filename);
+            return this;
+        }
+
+        public ResultModal shouldHaveAddress(String address) {
+            result.verifyResult(ADDRESS_LABEL, address);
+            return this;
+        }
+
+        public ResultModal shouldHaveStateAndCity(String state, String city) {
+            result.verifyResult(STATE_AND_CITY_LABEL, state + " " + city);
+            return this;
+        }
+        public void shouldNotBeVisible() {
+            result.shouldNotBeVisible();
+        }
     }
 }
