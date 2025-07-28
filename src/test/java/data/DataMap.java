22ea140a1c2ac2f2a10d1656b6e267cc63ef8abf
kolomiets.aa <kolomiets.aan@gazprom-neft.ru>
2025-07-28 11:44:46 +0300

    Вынес мапинг в отдельный файл

---
+++ src/test/java/data/DataMap.java
@@ -0,0 +1,25 @@
+package data;
+
+import java.util.List;
+import java.util.Map;
+
+public class DataMap {
+    public static final String uploadFile = "MyPhoto.jpg";
+
+    public static final String[] SUBJECT = {
+            "Accounting", "Maths", "Arts", "English", "Physics", "Chemistry",
+            "Computer Science", "Economics", "Social Studies", "History",
+            "Civics", "Commerce", "Hindi", "Biology"
+    };
+
+    public static final String[] HOBBIES = {
+            "Reading", "Sports", "Music"
+    };
+
+    public static final Map<String, String[]> mapStateWithCity = Map.of(
+            "NCR", new String[]{"Delhi", "Gurgaon", "Noida"},
+            "Uttar Pradesh", new String[]{"Agra", "Lucknow", "Merrut"},
+            "Haryana", new String[]{"Karnal", "Panipat"},
+            "Rajasthan", new String[]{"Jaipur", "Jaiselmer"}
+    );
+}
