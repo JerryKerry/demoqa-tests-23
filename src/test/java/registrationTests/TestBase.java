163adb531abbe4a7da2ada44e70261679fffee45
kolomiets.aa <kolomiets.aan@gazprom-neft.ru>
2025-07-28 11:47:26 +0300

    Убрал RegistrationPage

--- src/test/java/registrationTests/TestBase.java
+++ src/test/java/registrationTests/TestBase.java
@@ -1,9 +1,10 @@
+package registrationTests;
+
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
-import pages.RegistrationPage;
+
 
 public class TestBase {
-    RegistrationPage registrationPage = new RegistrationPage();
 
     @BeforeAll
     static void basicBrowserSettings() {
