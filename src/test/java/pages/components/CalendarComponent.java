a9506a46f2b28c5d7730d0d2ed5e8952f6b580af
kolomiets.aa <kolomiets.aan@gazprom-neft.ru>
2025-07-29 19:42:43 +0300

    вынес локаторы

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -1,11 +1,17 @@
 package pages.components;
 
+import com.codeborne.selenide.SelenideElement;
+
 import static com.codeborne.selenide.Selenide.$;
 
 public class CalendarComponent {
+        SelenideElement
+                selectMonth = $(".react-datepicker__month-select"),
+                selectYear = $(".react-datepicker__year-select");
+
     public void setDate(String day, String month, String year) {
-        $(".react-datepicker__month-select").selectOption(month);
-        $(".react-datepicker__year-select").selectOption(year);
+        selectMonth.selectOption(month);
+        selectYear.selectOption(year);
         $(".react-datepicker__day--0" + day + ":not(.react-datepicker__day--outside-month)").click();
 
     }
