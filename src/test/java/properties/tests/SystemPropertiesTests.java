4427534fdb32d6003690f8b643ada0df59edd8f6
kolomiets.aa <kolomiets.aan@gazprom-neft.ru>
2025-08-25 10:43:36 +0300

    поправил проперти

--- src/test/java/properties/tests/SystemPropertiesTests.java
+++ src/test/java/properties/tests/SystemPropertiesTests.java
@@ -6,13 +6,7 @@ import org.junit.jupiter.api.Test;
 public class SystemPropertiesTests {
 
     @Test
-    void systemPropertiesTest() {
-        String browser = System.getProperty("browser");
-
-        System.out.println(browser);
-    }
-
-    @Test
+    @Tag("property")
     void systemProperties1Test() {
         System.setProperty("browser", "chrome");
         String browser = System.getProperty("browser");
@@ -21,25 +15,9 @@ public class SystemPropertiesTests {
     }
 
     @Test
-    void systemProperties2Test() {
-        String browser = System.getProperty("browser", "mozilla");
-
-        System.out.println(browser);
-    }
-
-    @Test
-    void systemProperties3Test() {
-        System.setProperty("browser", "chrome");
-        String browser = System.getProperty("browser", "mozilla");
-
-        System.out.println(browser);
-    }
-
-    @Test
     @Tag("property")
     void systemProperties4Test() {
-        String browser = System.getProperty("browser", "mozilla");
-
+        String browser = System.getProperty("browser", "firefox");
         System.out.println(browser);
     }
 
